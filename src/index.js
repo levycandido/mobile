adccb6d21410d0297e2d6845cc05095b7a59150e
levycandido <31323642+levycandido@users.noreply.github.com>
2023-12-29 05:51:16 -0300

    fix the index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express = require('express'); //importa o módulo express dentro da constante express
-const control = require('./control')
 const documentClient = require('./dynamodbClient')
 const {ScanCommand} = require('@aws-sdk/lib-dynamodb')
 
 // O express será executado na constante app. 
 // app será um objeto de servidor que será usado para fazer várias coisas como 
-// por exemplo ligar o servidor.
+// por exemplo ligar o servidor
 const app = express();
 
 app.get('/', async (request, response) => {
